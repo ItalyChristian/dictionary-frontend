b74b5c3daed75c2ee7eb4399a3c98975baf4499c
italy <italy.christian@hotmail.com>
2026-07-07 03:02:31 -0300

    feature: create History page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,10 +1,8 @@
 import MainLayout from "@/templates/MainLayout";
-import { Header } from "@/components/Header";
 
 export default function History() {
   return (
     <MainLayout>
-      <Header />
       <span>Página de histórico</span>
     </MainLayout>
   );
