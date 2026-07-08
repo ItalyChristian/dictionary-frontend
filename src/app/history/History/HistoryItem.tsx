bb1e9e929f20478a467c7f0e3c6fd1b737a8f3c2
italy <italy.christian@hotmail.com>
2026-07-08 04:31:50 -0300

    feature: update FavoriteItem and HistoryItem to open word details on click

--- src/app/history/History/HistoryItem.tsx
+++ src/app/history/History/HistoryItem.tsx
@@ -2,12 +2,22 @@ import clsx from "clsx";
 import * as styles from "./styles.css";
 import { formatDate } from "@/utils/formatDate";
 import { HistoryItemProps } from "./types";
+import { useRouter } from "next/navigation";
 
 export function HistoryItem({ history }: HistoryItemProps) {
+  const router = useRouter();
+
   const formattedDate = formatDate(history.added);
 
+  const handleWordClick = (word: string) => {
+    router.push(`/word/${encodeURIComponent(history.word)}`);
+  };
+
   return (
-    <div className={clsx(styles.item)}>
+    <div
+      className={clsx(styles.item)}
+      onClick={() => handleWordClick(history.word)}
+    >
       <div className={styles.content}>
         <p className={styles.word}>{history.word}</p>
         <span className={styles.date}>Added: {formattedDate}</span>
